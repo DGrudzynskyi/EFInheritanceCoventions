f89f75f0b6f6d50976e70b497cd9715a161f0dfd
Danylo Hrudzynskyi <dan_gru@ukr.net>
2019-10-26 09:21:37 +0300

    restore original project state with TPH inheritance

--- EFExamples2/Schema/Parcel.cs
+++ EFExamples2/Schema/Parcel.cs
@@ -24,13 +24,13 @@ namespace EFExamples2.Schema
         public virtual Werehouse Werehouse { get; set; }
 
         // полиморфная ассоциация (TPH, TPT)
-        // public virtual List<Activity> Activities { get; set; }
+        public virtual List<Activity> Activities { get; set; }
 
         // не-полиморфная ассоциация (TPCT)
-        public List<CreateActivity> CreateActivities { get; set; }
-        public List<SendActivity> SendActivities { get; set; }
-        public List<ReadyForSendActivity> ReadyForSendActivities { get; set; }
-        public List<RetrieveActivity> RetriesveActivities { get; set; }
+        // public List<CreateActivity> CreateActivities { get; set; }
+        // public List<SendActivity> SendActivities { get; set; }
+        // public List<ReadyForSendActivity> ReadyForSendActivities { get; set; }
+        // public List<RetrieveActivity> RetriesveActivities { get; set; }
 
         public class ParcelConfiguration : EntityTypeConfiguration<Parcel>
         {
